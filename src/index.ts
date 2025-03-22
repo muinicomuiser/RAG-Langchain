1684b8d7f63857ed565143f26ad50d05aaa8a391
muinicomuiser <nicodoneg@gmail.com>
2025-03-21 22:37:25 -0300

    Lint config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from "express"
+import express from 'express'
 
 const app = express()
 app.use(express.json())
 
 const PORT = 3000
 
-app.get("/ping", (_req, res) => {
-  console.log("someone pinged here!!")
-  res.send("pong")
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here!!')
+  res.send('pong')
 })
 
 app.listen(PORT, () => {
---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+// Aquí recopilar los tipos y exportar
+// Ejemplo:
+// export type Color = "blanco" | "azul" | "rojo" | "verde"
+
+// Ejemplo
+// export interface Persona {
+//   nombre: string,
+//   edad: number,
+//   colorFavorito: Color
+// }
+// **el Midudev propone usar interfaces al extender
